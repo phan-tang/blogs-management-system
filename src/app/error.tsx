e889c28cdd47fea082d8cfb036ea0344d8881395
Tang Hien Phan <phan05042000@gmail.com>
2024-11-13 22:48:28 +0700

    Add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+export default function Error() {
+    return (
+        <section className="page">
+            <div className="page-content">
+                <h1>Error occured</h1>
+                <a href="/">
+                    Click here to Home page
+                </a>
+            </div>
+        </section>
+    );
+}
