277c2331d0e99d07965b6376f150e54547f6ec62
Tang Hien Phan <phan05042000@gmail.com>
2024-11-13 22:49:00 +0700

    Add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+    return (
+        <section className="page">
+            <div className="page-content">
+                <h1>Not found</h1>
+                <a href="/">
+                    Click here to Home page
+                </a>
+            </div>
+        </section>
+    );
+}
