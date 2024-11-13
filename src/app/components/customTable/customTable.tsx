72a3249cc8f2157ee52eebebb9b991b8583e1c59
Tang Hien Phan <phan05042000@gmail.com>
2024-11-13 22:58:32 +0700

    Add CustomTable component

---
+++ src/app/components/customTable/customTable.tsx
@@ -0,0 +1,34 @@
+import { Table } from "react-bootstrap";
+
+import styles from './customTable.module.scss';
+
+export default function CustomTable({ tableData, tableName }: { tableData: any[], tableName: string }) {
+    const tableHeaders = Object.keys(tableData[0]);
+    return (
+        <div className={styles.customTable}>
+            <Table responsive>
+                <thead>
+                    <tr>
+                        {tableHeaders.map((tableHeader, headerIndex) => {
+                            return (
+                                <th key={`${tableName}-table-header-${headerIndex}`}>{tableHeader}</th>
+                            );
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableData.map((tableItem, itemIndex) => {
+                        return (
+                            <tr key={`${tableName}-table-item-${itemIndex}`}>
+                                {tableHeaders.map((tableHeader, headerIndex) => {
+                                    return (
+                                        <td key={`${tableName}-table-item-${itemIndex}-${headerIndex}`}>{tableItem[tableHeader]}</td>
+                                    );
+                                })}
+                            </tr>);
+                    })}
+                </tbody>
+            </Table>
+        </div>
+    );
+}
