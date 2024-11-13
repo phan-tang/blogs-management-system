6d3310df389269deeec60477eb800b6c5b4d7cee
Tang Hien Phan <phan05042000@gmail.com>
2024-11-13 22:55:12 +0700

    Add pages for category

---
+++ src/app/categories/[slug]/page.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import { useEffect, useState, use } from 'react';
+
+import Loading from '../../loading';
+import Card from '../../components/card/card';
+import CustomPagination from '@/app/components/customPagination/customPagination';
+import { BlogItem } from '../../models/blogModel';
+import { getBlogs } from '@/app/services/blogService';
+import { toastError } from '@/app/lib/toastify';
+import { resizeGridRow } from '../../lib/plugins';
+import { categories, pageSize } from '@/app/lib/constants';
+import styles from './../categories.module.scss';
+
+export default function Category({ params }: { params: Promise<{ slug: string }> }) {
+    const [cardItems, setCardItems] = useState<BlogItem[]>([]);
+    const [blogsData, setBlogsData] = useState<BlogItem[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const category = categories.filter(item => item.toLowerCase() === use(params).slug)[0];
+
+    const resizeAllItemsGridRow = (): void => {
+        cardItems?.forEach((item, index) => {
+            let id: string = `categories-card-${index}`;
+            resizeGridRow(id);
+        })
+    }
+
+    const handleChangePage = (pageIndex: number): void => {
+        setLoading(true);
+        setCardItems(blogsData.slice(pageSize * (pageIndex - 1), pageSize * pageIndex));
+        setLoading(false);
+        window.scrollTo(0, 0);
+    }
+
+    useEffect(() => {
+        getBlogs().then(
+            (data) => {
+                let categoryBlogsData = data.filter((item) => item.catLinks.includes(category));
+                setBlogsData(categoryBlogsData);
+                setCardItems(categoryBlogsData.slice(0, pageSize));
+                setLoading(false);
+            }
+        ).catch((err) => {
+            toastError('Failed to load blogs data');
+        }
+        );
+    }, []);
+
+    return (
+        <>
+            {loading && <Loading />}
+            {!loading && <section>
+                <div className='page-header'>
+                    <div className="row">
+                        <div className="col-12">
+                            <h1>Category: {category}</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className={'with-top-sep ' + styles.categories}>
+                    <div className="masonry" onLoad={() => { resizeAllItemsGridRow() }}>
+                        {cardItems.map((item, index) => {
+                            return (
+                                <Card cardItem={item} id={`categories-card-${index}`} key={`categories-card-${index}`} />
+                            );
+                        })}
+                    </div>
+                </div>
+                <CustomPagination numberOfPages={Math.ceil(blogsData.length / pageSize)} handleChangePage={handleChangePage} />
+            </section>}
+        </>
+    );
+}
---
+++ src/app/categories/page.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+
+import Loading from '../loading';
+import Card from '../components/card/card';
+import CustomPagination from '../components/customPagination/customPagination';
+import { BlogItem } from '../models/blogModel';
+import { getBlogs } from '../services/blogService';
+import { toastError } from '../lib/toastify';
+import { resizeGridRow } from '../lib/plugins';
+import { pageSize } from '../lib/constants';
+import styles from './categories.module.scss';
+
+export default function Categories() {
+    const [cardItems, setCardItems] = useState<BlogItem[]>([]);
+    const [blogsData, setBlogsData] = useState<BlogItem[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const resizeAllItemsGridRow = () => {
+        cardItems?.forEach((item, index) => {
+            let id: string = `categories-card-${index}`;
+            resizeGridRow(id);
+        })
+    }
+
+    const handleChangePage = (pageIndex: number): void => {
+        setLoading(true);
+        setCardItems(blogsData.slice(pageSize * (pageIndex - 1), pageSize * pageIndex));
+        setLoading(false);
+        window.scrollTo(0, 0);
+    }
+
+    useEffect(() => {
+        getBlogs().then(
+            (data) => {
+                setBlogsData(data);
+                setCardItems(data.slice(0, pageSize));
+                setLoading(false);
+            }
+        ).catch((err) => {
+            toastError('Failed to load blogs data')
+        });
+    }, []);
+
+    return (
+        <>
+            {loading && <Loading />}
+            {!loading && <section>
+                <div className='page-header'>
+                    <div className="row">
+                        <div className="col-12">
+                            <h1>Categories</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className={'with-top-sep ' + styles.categories}>
+                    <div className="masonry" onLoad={() => { resizeAllItemsGridRow() }}>
+                        {cardItems.map((item, index) => {
+                            return (
+                                <Card cardItem={item} id={`categories-card-${index}`} key={`categories-card-${index}`} />
+                            );
+                        })}
+                    </div>
+                </div>
+                <CustomPagination numberOfPages={Math.ceil(blogsData.length / pageSize)} handleChangePage={handleChangePage} />
+            </section>}
+        </>
+    );
+}
