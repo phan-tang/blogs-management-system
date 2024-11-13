5fed36812bd67686c711f116793c7bd6955d2864
Tang Hien Phan <phan05042000@gmail.com>
2024-11-13 23:02:42 +0700

    Add page for users management

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import CustomTable from "@/app/components/customTable/customTable";
+import Loading from "@/app/loading";
+import CustomPagination from "@/app/components/customPagination/customPagination";
+import { UserInformation } from "@/app/models/userModel";
+import { adminPageSize } from "@/app/lib/constants";
+import { getUsers } from "@/app/services/userService";
+import { toastError } from "@/app/lib/toastify";
+import styles from './users.module.scss';
+
+
+export default function UsersManagement() {
+    const router = useRouter();
+    const [usersData, setUsersData] = useState<UserInformation[]>([]);
+    const [tableItems, setTableItems] = useState<UserInformation[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const handleChangePage = (pageIndex: number): void => {
+        setLoading(true);
+        setTableItems(usersData.slice(adminPageSize * (pageIndex - 1), adminPageSize * pageIndex));
+        setLoading(false);
+        window.scrollTo(0, 0);
+    }
+
+    useEffect(() => {
+        let user = sessionStorage.getItem('user');
+        if (!user) {
+            router.push('/auth/login');
+        }
+        getUsers().then(
+            (data) => {
+                setUsersData(data);
+                setTableItems(data.slice(0, adminPageSize));
+                setLoading(false);
+            }
+        ).catch((err) => {
+            toastError('Failed to load users data');
+        });
+    }, []);
+
+    return (
+        <>
+            {loading && <Loading />}
+            {!loading && <div className="page">
+                <div>
+                    <div className={styles.users}>
+                        <h3>Users Management</h3>
+                        <CustomTable tableData={tableItems} tableName="users" />
+                    </div>
+                    <CustomPagination numberOfPages={Math.ceil(usersData.length / adminPageSize)} handleChangePage={handleChangePage} />
+                </div>
+            </div>}
+        </>
+    );
+}
