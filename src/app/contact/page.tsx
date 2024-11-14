83d6417ea20483b1d3ef7f852b012f873d1f3856
Tang Hien Phan <phan05042000@gmail.com>
2024-11-15 00:23:36 +0700

    Remove unuse code

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -35,23 +35,6 @@ export default function Contact() {
                                         </p>
                                     </div>
                                 </div>
-                                {/* <form name="cForm" id="cForm" method="post" action="">
-                                <fieldset>
-                                    <div className="form-field">
-                                        <input name="cName" type="text" id="cName" className="full-width" placeholder="Your Name" value="" />
-                                    </div>
-                                    <div className="form-field">
-                                        <input name="cEmail" type="text" id="cEmail" className="full-width" placeholder="Your Email" value="" />
-                                    </div>
-                                    <div className="form-field">
-                                        <input name="cWebsite" type="text" id="cWebsite" className="full-width" placeholder="Website" value="" />
-                                    </div>
-                                    <div className="message form-field">
-                                        <textarea name="cMessage" id="cMessage" className="full-width" placeholder="Your Message" ></textarea>
-                                    </div>
-                                    <button type="submit" className="submit button-primary full-width-on-mobile">Submit</button>
-                                </fieldset>
-                            </form> */}
                             </div>
                         </section>
                     </div>
