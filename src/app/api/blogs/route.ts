5884110cd54946aaca8ed46e4431ebb28722adf7
Tang Hien Phan <phan05042000@gmail.com>
2024-11-15 00:20:52 +0700

    Modify APIs for searching data

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = 'force-static';
+export const dynamic = 'force-dynamic';
 
 export async function GET(req: NextRequest) {
+    let searchParam = new URL(req.url).search;
     let url = process.env.BACKEND_URL + '/posts';
-    const response = await fetch(url);
+    const response = await fetch(url + searchParam);
     const data = await response.json();
     return NextResponse.json({ data })
 }
